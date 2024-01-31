10f5448395b2152e4ff39d96eac53bc06fa89c55
sallyMohamed <engsally@gmail.com>
2024-01-31 03:36:51 +0200

    done

--- 0x01-challenge/user.py
+++ 0x01-challenge/user.py
@@ -1,30 +1,23 @@
 #!/usr/bin/python3
-""" 
+"""
 User class
 """
 
-class User():
-    """ Documentation """
+class User:
+    """ User class represents a user """
 
     def __init__(self):
-        """ Documentation """
+        """ Initializes a new User instance """
         self.__email = None
 
-    @email.setter
-    def email(self, value):
-        """ Documentation """
-        if type(value) is not str:
-            raise TypeError("email must be a string")
-        self.__email = value
+        @property
+        def email(self):
+            """ Getter method for the email attribute """
+            return self.__email
 
-    @property
-    def email(self):
-        """ Documentation """
-        return self.__email
-   
-    
-if __name__ == "__main__":
-
-    u = User()
-    u.email = "john@snow.com"
-    print(u.email)
+        @email.setter
+        def email(self, value):
+            """ Setter method for the email attribute """
+            if not isinstance(value, str):
+                raise TypeError("email must be a string")
+            self.__email = value
